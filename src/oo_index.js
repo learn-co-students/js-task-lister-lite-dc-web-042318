c1546802585fd58db9346b12dfbce2eed4719161
munirkizilcali <munir.kizilcali@gmail.com>
2018-06-07 16:14:16 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  taskList = new TaskList();
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', (e)=>{
+  	e.preventDefault()
+  	taskList.createTask(document.querySelector('#new-task-description').value)
+  	taskList.renderAll()
+  	form.querySelector('#new-task-description').value = ''
+  })
+
 });
--- src/task.js
+++ src/task.js
@@ -1,2 +1,18 @@
 class Task {
+	constructor(content, id) {
+		this.id = id
+		this.content = content
+	}
+
+	render() {
+		let listItem = document.createElement('li')
+		listItem.innerText = this.content + " "
+		listItem.id = this.id
+		let removeButton = document.createElement('button')
+		removeButton.innerText = 'X'
+		removeButton.addEventListener('click', ()=>{
+			taskList.deleteTask(this.id)})
+		listItem.appendChild(removeButton)
+		document.querySelector("#tasks").appendChild(listItem)
+	}
 }
