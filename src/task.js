ef5a9f662ae33111e7c400111f7540cf3ffaa6a6
joshpson <jpearson846@gmail.com>
2018-06-11 18:55:32 -0400

    Done.

--- src/task.js
+++ src/task.js
@@ -1,2 +1,16 @@
 class Task {
+  constructor(text) {
+    this.text = text;
+  }
+
+  listItem() {
+    let li = document.createElement("li");
+    li.innerText = this.text;
+    li.setAttribute("class", "task-item");
+    let button = document.createElement("button");
+    button.setAttribute("data-description", this.text);
+    button.innerText = "x";
+    li.appendChild(button);
+    return li;
+  }
 }
